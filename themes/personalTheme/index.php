924b6c1cb7cf7de24505f3d07cc2b24cbac80c8f
Vladys K <vldk0022@gmail.com>
2025-07-10 22:36:26 +0200

    webiste under construction done

--- themes/personalTheme/index.php
+++ themes/personalTheme/index.php
@@ -4,7 +4,7 @@ get_header();
 
 <div class="terminal">
   <div class="terminal__window">
-    <h1>This website is under construction</h1>
+    <h1>This website is under construction </h1>
     <img
         src="<?php echo get_template_directory_uri(); ?>/assets/under-construction.gif"
         alt="loading bar">
